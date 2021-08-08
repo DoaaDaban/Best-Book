2d6f04e1303f19057660512dbe525cde3b878f9b
DoaaDaban <doaadaban993@gmail.com>
2021-08-08 22:42:07 +0300

    lab11

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+import BestBooks from './BestBooks'
+import Profile from './Profile';
 import Footer from './Footer';
 import {
   BrowserRouter as Router,
@@ -8,23 +9,34 @@ import {
   Route
 } from "react-router-dom";
 
-class App extends React.Component {
 
+class App extends React.Component {
+  
   render() {
+  
     console.log('app', this.props);
-    return(
+  
+    return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {/* {isAuthenticated ? <BestBooks /> : <LoginButton />} */}
+                {/* <LogoutButton /> */}
+               <BestBooks />
+            </Route >
+
+            <Route exact path="/Profile">
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            {/* {isAuthenticated ? <Profile /> : <LoginButton />} */}
+            <Profile />
+            </Route>
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
